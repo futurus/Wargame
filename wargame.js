71f7e8ee1f48ca515561e87e190793a96c2bf113
futurus <futurus@live.com>
2015-03-23 16:32:12 -0400

    mock human vs AI moves

--- wargame.js
+++ wargame.js
@@ -1,88 +1,44 @@
 // YOUR CODE GOES HERE
-
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var aiThinking = false;
 // defind the Tile object
 var Tile = function(id, value) {
 	this.id = id;
 	this.value = value;
-	this.state = null;
+	this.belongsTo = null;
 };
-
-Tile.prototype.reveal = function() {
-		this.state = 'DISPLAYED';
-};
-	
-Tile.prototype.hide = function() {
-		this.state = 'HIDDEN';
-};
-	
-Tile.prototype.matched = function() {
-		this.state = 'MATCHED';
-};
-
 // end of Tile object definition
 
 // defind the Board object
-var Board = function(size) {
-	this.size = size;
-	this.tiles = [];
-	this.cTiles = [];
+var Board = function(p1, p2) {
+	this.p1 = p1;
+	this.p2 = p2;
+	this.p1tiles = [];
+	this.p2tiles = [];
+	this.turnCount = 0;
+
 };
 
-// symbols borrowed from https://github.com/zenahirsch/memory/
-Board.prototype.symbols = [
-		'&#9742;', '&#9792;', '&#9794;', '&#9733;', '&#9824;', '&#9827;', '&#9775;', '&#9822;',
-		'&#9730;', '&#10084;', '&#9986;', '&#9873;', '&#9773;', '&#65533;', '&#63819;', '&#63498;',
-		'&#63499;', '&#9829;', '&#9830;', '&#9834;', '&#10016;', '&#10003;', '&#10007;', '&#63491;',
-		'&#8540;', '&#8531;', '&#8859;', '&#9651;',	'&#1590;', '&#1334;', '&#936;', '&#9762;', 
-		'&#43742;',	'&#12354;'
-	];
-	
-Board.prototype.score = 0;
+Board.prototype.p1score = 0;
+Board.prototype.p2score = 0;
 
 Board.prototype.populate = function() {
 	// declare variables
 	var i,
-		id = 0,
-		numOfPairs = (this.size * this.size)/2,
-		randInd,
-		tempTile;
-	
+			numOfTiles = 36;
+
 	// initialize tiles
 	for (i = 0; i < numOfPairs; i++){
-		var t1 = new Tile(id  , this.symbols[i]);	this.tiles.push(t1);
-		var t2 = new Tile(id+1, this.symbols[i]);	this.tiles.push(t2);
-		id += 2;
-	}
-	
-	// randomize the tiles so the game is more interesting
-	for (i = 0; i < this.tiles.length; i++) {
-		// pick a random index that's not the current index
-		do {
-			randInd = Math.floor(Math.random() * this.tiles.length);
-		} while (randInd == i);
-		
-		tempTile = this.tiles[i];
-		this.tiles[i] = this.tiles[randInd];
-		this.tiles[randInd] = tempTile;
-	}
-	
-	// reset tile id so players can't cheat (view source)
-	for (i = 0; i < this.tiles.length; i++) {
-		this.tiles[i].id = i; // well, technically id should be private
-		this.tiles[i].hide();
+		var tile = new Tile(i, 1);	this.tiles.push(tile);
 	}
 };
 
 Board.prototype.checkWin = function() {
 	var i;
-	
-	for (i = 0; i < this.tiles.length; i++) {
-		if (this.tiles[i].state !== 'MATCHED') {
-			return false;
-		}
-	}
-	
-	return true;
+
+	return false;
 };
 
 Board.prototype.goLive = function() {
@@ -90,102 +46,55 @@ Board.prototype.goLive = function() {
 	var match = 0;
 
 	$('.tile').click(function () {
+		cBoard.turnCount += 1;
 		var id = $(this).attr('id');
-		
-		// this looks tedious but it works in the sense that it enforces
-		// that NOTHING happens while checking result
-		// otherwise, clicking quickly could leave 3 tiles open -> missing check
-        // and results in unfinishable games
-		if (cBoard.cTiles[0] === undefined) {
-            if (cBoard.tiles[id].state === 'HIDDEN') { // if it's HIDDEN, reveal it
-                cBoard.tiles[id].reveal();
-                $(this).html(cBoard.tiles[id].value);
-                cBoard.cTiles.push(cBoard.tiles[id]);
-                cBoard.score += 1;
-            }
-        } else {
-            if (cBoard.cTiles[1] === undefined) {
-                // if cTiles (current tiles) contains 2 elements, compare them
-                if (cBoard.tiles[id].state === 'HIDDEN') {
-                    cBoard.tiles[id].reveal();
-                    $(this).html(cBoard.tiles[id].value);
-                    cBoard.cTiles.push(cBoard.tiles[id]);
-                    cBoard.score += 1;
-                }
+		//console.log(id);
+		console.log(cBoard.turnCount);
 
-                var Tiles = cBoard.cTiles; // redundant, however makes it easier to refer to current tiles
-                if (Tiles[0].value === Tiles[1].value) {
-                    cBoard.tiles[Tiles[0].id].matched();
-                    $('#app-board').find('td#' + Tiles[0].id).css('background-color', 'lightgreen');
-                    cBoard.tiles[Tiles[1].id].matched();
-                    $('#app-board').find('td#' + Tiles[1].id).css('background-color', 'lightgreen');
-                    match += 2;
-                    cBoard.cTiles = [];
-                } else {
-                    $('#app-board').find('td#' + Tiles[0].id).css('background-color', 'red');
-                    $('#app-board').find('td#' + Tiles[1].id).css('background-color', 'red');
-                    setTimeout(function () {
-                        $('#app-board').find('td#' + Tiles[0].id).html('');
-                        $('#app-board').find('td#' + Tiles[0].id).css('background-color', 'lightgray');
-                        $('#app-board').find('td#' + Tiles[1].id).html('');
-                        $('#app-board').find('td#' + Tiles[1].id).css('background-color', 'lightgray');
+		if (cBoard.turnCount % 2 == 1) { // player 1
+			if (!aiThinking) {
+				$(this).css('backgroundColor', '#FF0000');
 
-                        cBoard.cTiles = [];
+				// trigger AI's move
+				aiThinking = true;
+				move = getRandomInt(1, 36);
+				$("#app-board").find("td#" + move).trigger('click');
+			} else {
+				$('#app-message').html(cBoard.p2 + ' has not completed her move. Please wait!');
+			}
 
-                        cBoard.tiles[Tiles[0].id].hide();
-                        cBoard.tiles[Tiles[1].id].hide();
-                    }, 750);
-                }
-            }
+		} else { // player 2
+			setTimeout(function() {
+				$("#app-board").find("td#" + id).css('backgroundColor', '#6699FF');
+				$('#app-message').html(cBoard.p2 + ' has chosen ' + id);
+				aiThinking = false;
+			}, 1000);
 		}
 
-        $('#app-score').html(Math.floor((match/cBoard.score)*100) + '');
-        //$('#app-message').html(match + ' ' + cBoard.score);
-
-		// check if whole board is revealed, this is fired up every time there's a click, is there a way to do this more efficiently? 
-		// maybe put this in checkWin with a condition?
 		if (cBoard.checkWin()) {
 			$('#app-message').html('You won!');
 		}
-
 	});
 };
 
 Board.prototype.draw = function() {
-	var tInd = 0;
-	var html = '';
-
-	// constructing row for gameboard
-	for (var i = 0; i < this.size; i++) {
-		html += '<tr>';
-		// constructing column in each row
-		for (var x = 0; x < this.size; x++) {
-			html += '<td class="tile" id="' + this.tiles[tInd].id + '">' +
-				   '</td>';
-			tInd += 1;
-		}
-		html += '</tr>';
-	}
-
-	// update table with html code
-	$('#app-board').html(html);
-
 	// Add the event listeners
 	this.goLive();
 };
 // end of Board object definition
 
 // actual app
-$(function() {	
+$(function() {
 	$('#app-button').click(function() {
-		var size = $('#app-select').val();
-		var board = new Board(size);
-		
-		board.populate();
-		board.draw();
+		var p1 = $('#app-player1').val();
+		var p2 = $('#app-player2').val();
+		var board = new Board(p1, p2);
 
-        $('#app-score').html('0');
-		$('#app-message').html('&nbsp;');
+		//board.populate();
+		board.draw();
+		console.log(board.turnCount);
+		console.log(aiThinking);
+		$('#app-message').html('Game between ' + p1 + ' and ' + p2 + ' started!');
 	});
 });
 // end of file
